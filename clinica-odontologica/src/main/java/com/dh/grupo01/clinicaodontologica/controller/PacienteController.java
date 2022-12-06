5e0d0f505aaa97a5b57e7931fb5d805e6304bc34
bczeulli <bczeulli@gmail.com>
2022-12-06 09:13:05 -0300

    throws no controller paciente

--- clinica-odontologica/src/main/java/com/dh/grupo01/clinicaodontologica/controller/PacienteController.java
+++ clinica-odontologica/src/main/java/com/dh/grupo01/clinicaodontologica/controller/PacienteController.java
@@ -2,6 +2,8 @@ package com.dh.grupo01.clinicaodontologica.controller;
 
 import com.dh.grupo01.clinicaodontologica.entity.Paciente;
 import com.dh.grupo01.clinicaodontologica.entity.dto.PacienteDTO;
+import com.dh.grupo01.clinicaodontologica.exception.CadastroInvalidoException;
+import com.dh.grupo01.clinicaodontologica.exception.ResourceNotFoundException;
 import com.dh.grupo01.clinicaodontologica.service.impl.PacienteIMPLService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,32 +20,32 @@ public class PacienteController {
     PacienteIMPLService service ;
 
     @GetMapping()
-    public List<PacienteDTO> buscar(){
+    public List<PacienteDTO> buscar() {
         return service.buscar();
     }
 
     @GetMapping("/buscarCpf/{cpf}")
-    public ResponseEntity buscarPorCpf(@PathVariable String cpf){
+    public ResponseEntity buscarPorCpf(@PathVariable String cpf) throws ResourceNotFoundException{
         return service.buscarPorCpf(cpf);
     }
     @PostMapping()
-    public ResponseEntity salvar(@RequestBody @Valid PacienteDTO pacienteDTO){
+    public ResponseEntity salvar(@RequestBody @Valid PacienteDTO pacienteDTO) throws CadastroInvalidoException {
         return service.salvar(pacienteDTO);
     }
 
     @DeleteMapping()
-    public ResponseEntity deletar(@RequestParam("cpf") String cpf){
+    public ResponseEntity deletar (@RequestParam("cpf") String cpf) throws ResourceNotFoundException{
         return service.deletar(cpf);
     }
 
     @PutMapping()
-    public ResponseEntity atualizarTotal(@RequestBody PacienteDTO pacienteDTO){
+    public ResponseEntity atualizarTotal(@RequestBody PacienteDTO pacienteDTO) throws ResourceNotFoundException{
         return service.atualizarTotal(pacienteDTO);
     }
 
 
     @PatchMapping()
-    public ResponseEntity atualizarParcial(@RequestBody PacienteDTO pacienteDTO){
+    public ResponseEntity atualizarParcial(@RequestBody PacienteDTO pacienteDTO) throws ResourceNotFoundException{
         return service.atualizarParcial(pacienteDTO);
     }
 
